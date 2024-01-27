b1ef5c301f1b53be5710c16d42f4bd57a1fa8f8c
haibobo1477 <hazhao@siue.edu>
2024-01-26 20:41:16 -0600

    Add pub.sh script

--- src/etc/scripts/pub.sh
+++ src/etc/scripts/pub.sh
@@ -13,5 +13,6 @@ ros2 topic pub $opt $topic_name $msg_type "$msg"
 
 
 # Example usage:
-generate_instant_publisher "--rate 100" "/drone/pose" "geometry_msgs/msg/Pose" "{position: {x: 2.0, y: 0.0, z: 0.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 1.8}}"
 
+generate_instant_publisher "--once" "/turtle1/cmd_vel" "geometry_msgs/msg/Twist" "{linear: {x: 0.0, y: 4.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -0.0}}"
+generate_instant_publisher "--once" "/turtle1/cmd_vel" "geometry_msgs/msg/Twist" "{linear: {x: 0.0, y: 5.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -5.5}}"
