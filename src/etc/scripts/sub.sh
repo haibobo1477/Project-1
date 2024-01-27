cbe89199b92d7f76cb65d8d10838598fba158fa8
haibobo1477 <hazhao@siue.edu>
2024-01-26 23:03:52 -0600

    add sub.sh script

--- src/etc/scripts/sub.sh
+++ src/etc/scripts/sub.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-subscribe_and_write_csv() {
-    # Ask for the <topic_name>
-    read -p "Type the topic_name: " topic_name
 
-    # Ask for the <file_name> CVS file
-    read -p "Type the file_name.csv: " file_name
+# Ask for the <topic_name>
+read -p "Type the topic_name: " topic_name
 
-    # Retrieve the rate at which data is published
-    read -p "Enter the rate at which data is published: " rate
+# Ask for the <file_name> CVS file
+read -p "Type the file_name.cvs:" file_name
 
-    # Write all the topics of the previous publisher to <file_name>.cvs
-    echo "Topic Name,Message Type" > "$file_name"
-    
-    # Placeholder for demonstration
-    # You need to replace this with actual logic to retrieve topics from the previous publisher
-    echo "$topic_name,geometry_msgs/msg/Pose" >> "$file_name"
-    echo "Topics written to $file_name"
+touch "$file_name"
 
-    # Placeholder for demonstration
-    echo "Subscribing to $topic_name with rate $rate..."
-    # Add your actual logic for subscribing to the specified topic with the given rate
+
+ros2 topic pub --rate 10 $topic_name geometry_msgs/msg/pose
+
+
+
+sub(){
+local topic_name
+local file_name
+
+echo "$topic Name" > "$file_name"
+nano $file.name
+
 }
 
-# Call the function
-subscribe_and_write_csv
+sub
+
